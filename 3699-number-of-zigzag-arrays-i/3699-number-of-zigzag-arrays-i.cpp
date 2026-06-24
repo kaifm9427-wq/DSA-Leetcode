c60fe090f0f135b84a92af364536d785cd699b77
Mohammed Kaif <kaifm9427@gmail.com>
2026-06-24 19:18:54 +0530

    Time: 271 ms (75.68%), Space: 11.9 MB (74.77%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,42 +1,17 @@
 class Solution {
 public:
+    static constexpr int MOD = 1000000007;
     int zigZagArrays(int n, int l, int r) {
-        const int MOD = 1e9 + 7;
         int m = r - l + 1;
+        vector<int> dp(m, 1);
 
-        if (n == 1) return m;
-
-        vector<int> up(m), down(m);
-
-        for (int i = 0; i < m; i++) {
-            up[i] = i;
-            down[i] = m - 1 - i;
+        for (int i = 2; i <= n; i++) {
+            reverse(dp.begin(), dp.end());
+            int sum = 0;
+            for (auto& d : dp)
+                sum = (sum + exchange(d, sum)) % MOD;
         }
 
-        for (int len = 3; len <= n; len++) {
-            vector<int> prefUp(m + 1), prefDown(m + 1);
-
-            for (int i = 0; i < m; i++) {
-                prefUp[i + 1] = (prefUp[i] + up[i]) % MOD;
-                prefDown[i + 1] = (prefDown[i] + down[i]) % MOD;
-            }
-
-            vector<int> newUp(m), newDown(m);
-
-            for (int i = 0; i < m; i++) {
-                newUp[i] = prefDown[i];
-                newDown[i] = (prefUp[m] - prefUp[i + 1] + MOD) % MOD;
-            }
-
-            up = newUp;
-            down = newDown;
-        }
-
-        long long ans = 0;
-        for (int i = 0; i < m; i++) {
-            ans = (ans + up[i] + down[i]) % MOD;
-        }
-
-        return ans;
+        return ((accumulate(dp.begin(), dp.end(), 0LL) % MOD) << 1) % MOD;
     }
 };
