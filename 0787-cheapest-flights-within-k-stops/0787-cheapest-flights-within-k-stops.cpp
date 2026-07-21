c1b8d1e3562cf6714ae242cb8d56c5464722e868
Mohammed Kaif <kaifm9427@gmail.com>
2026-07-22 00:05:10 +0530

    Time: 0 ms (100%), Space: 18.8 MB (22.49%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,54 +1,39 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights,
-                          int src, int dst, int k) {
-
-        vector<pair<int,int>> adj[n];
-
-        for (auto &it : flights) {
-            int u = it[0];
-            int v = it[1];
-            int price = it[2];
-
-            adj[u].push_back({v, price});
+    typedef pair<int,pair<int,int>> P;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it:flights){
+            int u=it[0];
+            int v=it[1];
+            int w=it[2];
+            adj[u].push_back({v,w});
         }
-
-        queue<pair<int, pair<int,int>>> q;
-
-        q.push({0, {src, 0}});
-
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
-
-        while (!q.empty()) {
-
-            int stops = q.front().first;
-            int node = q.front().second.first;
-            int cost = q.front().second.second;
-
-            q.pop();
-
-            if (stops > k)
-                continue;
-
-            for (auto &it : adj[node]) {
-
-                int adjNode = it.first;
-                int wt = it.second;
-
-                if (cost + wt < dist[adjNode]) {
-
-                    dist[adjNode] = cost + wt;
-
-                    q.push({stops + 1,
-                           {adjNode, cost + wt}});
+        priority_queue<P,vector<P>,greater<P>> pq;
+        pq.push({0,{src,k+1}});
+        vector<vector<int>> dist(n,vector<int>(k+2,INT_MAX));
+        dist[src][k+1]=0;
+        while(!pq.empty()){
+            int d=pq.top().first;
+            int node=pq.top().second.first;
+            int stop=pq.top().second.second;
+            pq.pop();
+            if(node==dst){
+                return d;
+            }
+            if(d>dist[node][stop]) continue;
+            if(stop==0) continue;
+            if(stop){
+                for(auto it:adj[node]){
+                    int newD=it.second;
+                    int newNode=it.first;
+                    if(newD+d<dist[newNode][stop-1]){
+                        dist[newNode][stop-1]=d+newD;
+                        pq.push({d+newD,{newNode,stop-1}});
+                    }
                 }
             }
         }
-
-        if (dist[dst] == INT_MAX)
-            return -1;
-
-        return dist[dst];
+        return -1;
     }
 };
