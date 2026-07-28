de21c3dc07a23fc333f3d1c26bf05bb5cba0375b
Mohammed Kaif <kaifm9427@gmail.com>
2026-07-28 23:54:32 +0530

    Time: 173 ms (10.33%), Space: 54 MB (95.38%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n=s.size();
+        int pivot=n/2;
+        sort(s.begin(),s.begin()+pivot);
+        sort(s.end()-pivot,s.end());
+        reverse(s.end()-pivot,s.end());
+        return s;
+
+    }
+};
