74b9b7b3819eab8a30b8da43f41596c83e306396
Mohammed Kaif <kaifm9427@gmail.com>
2026-07-31 00:10:41 +0530

    Time: 7 ms (65.94%), Space: 11.7 MB (79.92%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int n;
-    int dp[]
+    int dp[21][2001];
     int func(int ind,vector<int>& nums,int target, int k){
         if(ind>=n){
             if(target==k) return 1;
             return 0;
         }
-        
+        if(dp[ind][target+1000]!=-1) return dp[ind][target+1000];
         int pickPlus=func(ind+1,nums,target-nums[ind],k);
         int pickMinus=func(ind+1,nums,target+nums[ind],k);
-        return pickPlus+pickMinus;
+        return dp[ind][target+1000]=pickPlus+pickMinus;
 
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         n=nums.size();
+        memset(dp,-1,sizeof(dp));
         return func(0,nums,0,target);
     }
 };
