c124866892f3328178febca8f6bc1e904f984ceb
Mohammed Kaif <kaifm9427@gmail.com>
2026-08-19 10:52:07 +0530

    Time: 0 ms (100%), Space: 20.1 MB (46.5%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,7 +5,7 @@ public:
         int pickmin=INT_MAX;
         int pickmax=INT_MIN;
         int sell=0;
-        int sum=0;
+        int profit=0;
         int i=0;
         while(i<n){
             if(nums[i]<pickmin){
@@ -16,13 +16,13 @@ public:
             }
             if(pickmin!=INT_MAX && pickmax!=INT_MIN){
                 sell=pickmax-pickmin;
-                sum+=sell;
+                profit+=sell;
                 pickmax=INT_MIN;
                 pickmin=nums[i];
             }
             i++;
         }
-        return sum;
+        return profit;
 
     }
 };
